52ecf6a72502628b477a491e724edb18643e2ec4
davidcinca <davidcinca@gmail.com>
2013-08-25 18:46:32 +0200

    trying JRuby on Heroku

---
+++ programacio/rails/jruby/merchant/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Merchant::Application.config.secret_key_base = '521b38bedcdca5dfbf22bbd44c216e37874d4ae91110820c8da8e3fc334402a5c1334cd62278dfc88f09918bc962f06942e001fbb9054bb80345feb237f3f626'
